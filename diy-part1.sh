cd3ab7faa7054d3daa19df42630eb3cc04676708
kkshen1 <895636053@qq.com>
2020-06-20 12:03:29 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,6 +13,6 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
-git clone https://github.com/Lienol/openwrt-package package
+cd package
+git clone https://github.com/Lienol/openwrt-package 
 
